d6f808bb41939f31e2d35fd791da7a34a81e15f1
PretzelionSmol <koreanloverkyuu@gmail.com>
2020-03-21 15:18:27 +0000

    initial commit

--- main.cs
+++ main.cs
@@ -1,7 +1,27 @@
 using System;
+using static System.Console;
 
 class MainClass {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+   public static void Main (string[] args) {
+     while(true){
+     WriteLine("[Player 1] please pick your type:\n(1) rock\n(2) paper\n(3) scissors");
+     string ChoiceOfTheFirstPlayer = ReadLine();
+     int ChoiceOfTheFirstPlayerInt;
+     //probuje zmienic wartosc wczytana na int
+     while(!Int32.TryParse(ChoiceOfTheFirstPlayer, out ChoiceOfTheFirstPlayerInt)
+     ||ChoiceOfTheFirstPlayer > 3
+     ||ChoiceOfTheFirstPlayer <=0 ){
+       WriteLine("Give the right number");
+       ChoiceOfTheFirstPlayer = ReadLine();
+       }
+       WriteLine("[Player 2] please pick your type:\n(1) rock\n(2) paper\n(3) scissors");
+       string ChoiceOfTheSecondPlayer = ReadLine();
+       if (ChoiceOfTheFirstPlayer == ChoiceOfTheSecondPlayer){
+       WriteLine ("Draw");
+       }
+     WriteLine("Press any key to restart");
+     ReadKey();
+     Clear();
+     }
+   }
 }
