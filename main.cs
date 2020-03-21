ba1b3cd52d86eb3cff055ab6a9e17fff3608e2a4
iamarebel <terrorystka.aga@gmail.com>
2020-03-21 18:00:02 +0000

    gdzies na poczatku bałaganu...

--- main.cs
+++ main.cs
@@ -1,7 +1,16 @@
 using System;
+using static System.Console;
 
 class MainClass {
   public static void Main (string[] args) {
- gggggggg
+    string poczatekrozgrywki = "Wybierz jedną opcje:\n(1)kamień:\n(2) papier:\n(3) nożyczki";
+    WriteLine(poczatekrozgrywki);
+    string InputPlayerOne;
+    inputPlayerOne = ReadLine();
+    if(inputPlayerOne =="1"){
+      WriteLine("gracz wybrał kamień");
   }
 }
+    
+
+
