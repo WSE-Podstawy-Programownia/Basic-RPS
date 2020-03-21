4d4b94af8db25161315136927775318073e64bb3
VeresWSE <piotrw2.0@gmail.com>
2020-03-21 16:35:04 +0000

    Projekt kamien papier nozyce - test

--- main.cs
+++ main.cs
@@ -1,7 +1,33 @@
 using System;
+using static System.Console;
 
 class MainClass {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+    WriteLine ("[Gracz 1] wybierz symbol:\n(1) kamien\n(2) papier\n(3) nozyce");
+    string choiseOfThePlayerOne = ReadLine();
+    int choiseOfThePlayerOneInt;
+//
+while(!Int32.TryParse(choiseOfThePlayerOne, out choiseOfThePlayerOneInt)
+||choiseOfThePlayerOneInt > 3
+||choiseOfThePlayerOneInt <= 0 ){
+  WriteLine("Podaj poprawna liczbe");
+  choiseOfThePlayerOne = ReadLine();
+}
+
+WriteLine("[Gracz 2] wybierz symbol:\n(1) kamien\n(2) papier\n(3) nozyce");
+string choiseOfThePlayerTwo = ReadLine();
+int choiseOfThePlayerTwoInt;
+
+while(!Int32.TryParse(choiseOfThePlayerTwo, out choiseOfThePlayerTwoInt)
+||choiseOfThePlayerTwoInt > 3
+||choiseOfThePlayerTwoInt <= 0 ){
+  WriteLine("Podaj poprawna liczbe");
+  choiseOfThePlayerTwo = ReadLine();
+}
+
+if (choiseOfThePlayerOne == choiseOfThePlayerTwo){
+  WriteLine ("Remis");
+}
+
+  }  
 }
