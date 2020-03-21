bab21d38fdf75373eb836b6c84695f156af87357
Melbourne6 <veefff6@gmail.com>
2020-03-21 15:27:22 +0000

    fix

--- main.cs
+++ main.cs
@@ -5,6 +5,7 @@ class MainClass {
     Console.WriteLine ("Welcome! You're playing scissors,paper,rock game! Game rules: Scissors beat paper, paper beat rock, rock beat scissors. The game ends when you beat an opponent three times. Good luck! Ready! Steady! Go!");
     //Console.Clear();
     Console.WriteLine("[Gracz 1] Choose your type:\n(1) rock\n(2) paper\n(3) scissors");
+    
 
     int FirstPlayerChoose;
     while()
