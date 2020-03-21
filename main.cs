fb4f642892477a0d7fc034f9665049e2485da8ce
Karolina <kkpiwowarczyk@gmail.com>
2020-03-21 15:54:41 +0100

    Zmienione wartości ze string na int

--- main.cs
+++ main.cs
@@ -7,81 +7,74 @@ class MainClass
         bool keepPlaying = true;
         while (keepPlaying)
         {
-            Console.WriteLine("Wpisz: 'papier', 'kamieñ' lub 'no¿yce', a nastêpnie wciœnij ENTER");
-            string userChoice = Console.ReadLine();
+            Console.WriteLine("Wybierz: 1 - papier, 2 - kamieñ lub 3 - no¿yce, a nastêpnie wciœnij ENTER");
+            if (Int32.TryParse(Console.ReadLine(), out int userChoice) == false || userChoice < 1 || userChoice > 3)
+            {
+                Console.WriteLine("Musisz wybraæ 1 - papier, 2 - kamieñ lub 3 - no¿yce!");
+                continue;
+            }
 
             Random rnd = new Random();
             int computerChoice = rnd.Next(1, 4);
             {
                 if (computerChoice == 1)
                 {
-                    if (userChoice == "papier")
+                    if (userChoice == 1)
                     {
                         Console.WriteLine("Komputer wybra³ papier");
                         Console.WriteLine("Remis!");
                     }
-                    else if (userChoice == "kamieñ")
+                    else if (userChoice == 2)
                     {
                         Console.WriteLine("Komputer wybra³ kamieñ");
                         Console.WriteLine("Remis!");
                     }
-                    else if (userChoice == "no¿yce")
+                    else if (userChoice == 3)
                     {
                         Console.WriteLine("Komputer wybra³ no¿yce");
                         Console.WriteLine("Remis!");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Musisz wpisaæ 'papier', 'kamieñ' lub 'no¿yce'!");
                     }
                 }
                 else if (computerChoice == 2)
                 {
-                    if (userChoice == "papier")
+                    if (userChoice == 1)
                     {
                         Console.WriteLine("Komputer wybra³ kamieñ");
                         Console.WriteLine("Wygrana! Papier pokonuje kamieñ");
                     }
-                    else if (userChoice == "kamieñ")
+                    else if (userChoice == 2)
                     {
                         Console.WriteLine("Komputer wybra³ no¿yce");
                         Console.WriteLine("Wygrana! Kamieñ pokonuje no¿yce");
                     }
-                    else if (userChoice == "no¿yce")
+                    else if (userChoice == 3)
                     {
                         Console.WriteLine("Komputer wybra³ papier");
                         Console.WriteLine("Wygrana! No¿yce pokonuj¹ papier");
                     }
-                    else
-                    {
-                        Console.WriteLine("Musisz wpisaæ 'papier', 'kamieñ' lub 'no¿yce'!");
-                    }
                 }
                 else if (computerChoice == 3)
                 {
-                    if (userChoice == "papier")
+                    if (userChoice == 1)
                     {
                         Console.WriteLine("Komputer wybra³ no¿yce");
                         Console.WriteLine("Przegrana! No¿yce pokonuj¹ papier");
                     }
-                    else if (userChoice == "kamieñ")
+                    else if (userChoice == 2)
                     {
                         Console.WriteLine("Komputer wybra³ papier");
                         Console.WriteLine("Przegrana! Papier pokonuje kamieñ");
                     }
-                    else if (userChoice == "no¿yce")
+                    else if (userChoice == 3)
                     {
                         Console.WriteLine("Komputer wybra³ kamieñ");
                         Console.WriteLine("Przegrana! Kamieñ pokonuje no¿yce");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Musisz wpisaæ 'papier', 'kamieñ' lub 'no¿yce'!");
                     }
                 }
                 Console.WriteLine("Chcesz zagraæ jeszcze raz? t/n");
                 ConsoleKeyInfo cki = Console.ReadKey(); //wait for player to press a key
                 keepPlaying = cki.KeyChar == 't'; //continue only if y was pressed
+                Console.Clear();
             }
         }
     }
