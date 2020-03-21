8f7e54a47d0a438e6cdb311699fe4ffac831544b
Bella117 <izabela.gore@gmail.com>
2020-03-21 15:22:46 +0000

    Pierwsza wersja

--- main.cs
+++ main.cs
@@ -1,7 +1,22 @@
 using System;
+using static System.Console;
 
 class MainClass {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+     WriteLine ("Witaj w grze RPS");
+
+     
+  
+  System.Console.Clear();
+  WriteLine ("[Gracz 1] proszę podać swój typ:\n(1) kamień\n(2) papier\n(3)");
+  string wyborPierszegoGracza = ReadLine();
+  
+  System.Console.Clear();
+
+  WriteLine ("[Gracz2] proszę podać swój typ:\n(1) kamień\n(2) papier\n(3)");
+  string WybórDrugiegoGracza = ReadLine();
+  if (wyborPierszegoGracza == WybórDrugiegoGracza){
+    WriteLine ("Remis");
+    }
   }
 }
