1bcd52d7dc073ad181c9fb873f2676332a6c307f
iamarebel <terrorystka.aga@gmail.com>
2020-03-22 12:22:13 +0000

    podejscie  nr4 przerwy i dodatkowy loop continue po 1 graczu

--- main.cs
+++ main.cs
@@ -3,57 +3,61 @@ using static System.Console;
 class MainClass {
   public static void Main (string[] args) {
      while (true){
-      string poczatekrozgrywki = "Gra Papier, Kamień, Nożyce. Witaj Graczu NUMER 1! Wybierz jedną z opcji: \n1 - Papier \n2 - Kamień \n3 - Nożyce";
+      string poczatekrozgrywki = "\n \n Gra Papier, Kamień, Nożyce.\n \n Witaj Graczu NUMER 1! Wybierz jedną z opcji:\n \n1 - Papier \n2 - Kamień \n3 - Nożyce";
       WriteLine(poczatekrozgrywki);
       string InputPlayerOne;
       InputPlayerOne = ReadLine();
 
       if(InputPlayerOne == "1"){
-        WriteLine("wybrałeś PAPIER");
+        WriteLine("wybrałeś PAPIER \n");
       }
       else if(InputPlayerOne =="2"){
-        WriteLine("wybrałeś KAMIEŃ");
+        WriteLine("wybrałeś KAMIEŃ \n");
       }
       else if(InputPlayerOne =="3"){
-        WriteLine("wybrałeś NOŻYCE");
+        WriteLine("wybrałeś NOŻYCE \n");
       }
       else{
-        WriteLine("NIEPOPRAWNY WYBÓR!");
+        WriteLine("NIEPOPRAWNY WYBÓR!\n");
+        continue;
       }
-      WriteLine("Witaj Graczu NUMER 2! Wybierz jedną z opcji: \n1 - Papier \n2 - Kamień \n3 - Nożyce");
+      WriteLine("Witaj Graczu NUMER 2! Wybierz jedną z opcji:\n \n1 - Papier \n2 - Kamień \n3 - Nożyce");
       string InputPlayerTwo;
       InputPlayerTwo = ReadLine();
 
       if(InputPlayerTwo == "1"){
-        WriteLine("wybrałeś PAPIER");
+        WriteLine("wybrałeś PAPIER \n");
       }
       else if(InputPlayerTwo =="2"){
-        WriteLine("wybrałeś KAMIEŃ");
+        WriteLine("wybrałeś KAMIEŃ \n");
       }
       else if(InputPlayerTwo =="3"){
-        WriteLine("wybrałeś NOŻYCE");
+        WriteLine("wybrałeś NOŻYCE \n");
       }
       else{
-        WriteLine("NIEPOPRAWNY WYBÓR!");
+        WriteLine("NIEPOPRAWNY WYBÓR!\n");
       }
+      WriteLine("Naciśnij cokolwiek żeby poznać wynik rozgrywki\n");
+      Read();
+
       if(InputPlayerOne == InputPlayerTwo){
-        WriteLine("Jest REMIS!");
+        WriteLine("Jest REMIS!\n");
       }
       else if((InputPlayerOne == "1" && InputPlayerTwo == "3") 
         || (InputPlayerOne == "2" && InputPlayerTwo == "1")
         || (InputPlayerOne == "3" && InputPlayerTwo == "2")){
-          WriteLine("graczu PIERWSZY zwyciężyłeś!");
+          WriteLine("graczu PIERWSZY zwyciężyłeś!\n\n");
           }
       else if((InputPlayerOne == "3" && InputPlayerTwo == "1") 
         || (InputPlayerOne == "1" && InputPlayerTwo == "2")
         || (InputPlayerOne == "2" && InputPlayerTwo == "3")){
-          WriteLine("Graczu DRUGI zwyciężyłeś!");
+          WriteLine("Graczu DRUGI zwyciężyłeś!\n\n");
           }
       else{
-        WriteLine("Ktoś dokonał niepoprawnego wyboru!");
+        WriteLine("Ktoś dokonał niepoprawnego wyboru!\n\n");
         continue;
         }
-      WriteLine("Czy chcesz zakonczyc? [y]");
+      WriteLine("Naciśnij y jeśli chcesz zakończyć");
       string isExit = ReadLine();
       if(isExit == "y"){
         break;
