bf3a9f39ec91a9506ea6a94189f7465a0688c1b7
kacbor <borowski.kacper@gmail.com>
2020-03-25 12:07:35 +0000

    ver2

--- main.cs
+++ main.cs
@@ -2,6 +2,19 @@ using System;
 
 class MainClass {
   public static void Main (string[] args) {
-    Console.WriteLine ("goodbye---------- world");
+    Console.WriteLine ("Start Game");
+    System.Console.Clear();
+
+
+
+    Console.WriteLine (" [Gracz 1] proszę podać swoj typ: (1)kamień   (2)papier   (3) nożyce");
+    string wybor1gracza = ReadLine();
+    while (!Int32.TryParse(wybor1gracza, out wybor1graczaInt))
+
+    
+
+    Console.WriteLine("[Gracz 2] proszę podać swój typ:  (1)kamień   (2)papier   (3)nożyce");
+    string wybor2gracza = ReadLine();
+    public static string ReadLine() 
   }
 }
