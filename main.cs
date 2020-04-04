47cec0e7ad1746891457bdb727969109b8e353db
HarridanEll <62474125+HarridanEll@users.noreply.github.com>
2020-04-04 16:22:40 +0200

    Update main.cs
    zadanie 2 i początek zadania 3 - repl.it nie nadpisuje tu wszystkich komentarzy jakie wysyłam...

--- main.cs
+++ main.cs
@@ -12,33 +12,36 @@ class MainClass {
     WriteLine ("\n [push any key to continue]");
     ReadKey ();
     Clear ();
-     while (true) {
+    // funkja do ... while
+     do {
+      Clear ();
 
      //deklaracja player 1
      WriteLine("[Player 1] choose your type:\n(1) Rock\n(2) Paper\n(3) Scissors");
      string player1choose = ReadLine();
      int player1chooseInt;
-  
+
+      // wybierz poprawną liczbę
      while(!Int32.TryParse(player1choose, out player1chooseInt)
      || player1chooseInt > 3
      || player1chooseInt <=0 ){
        WriteLine("Select correct number");
        player1choose = ReadLine();}
-     string player1chooseString = Console.ReadLine();
-     gamesRecord[gamesRecordCurrentIndex, 0] = player1chooseString;
+     gamesRecord[gamesRecordCurrentIndex, 0] = player1choose;
      Clear ();
 
      //deklaracja player 2
      WriteLine("[Player 2] choose your type:\n(1) Rock\n(2) Paper\n(3) Scissors");
      string player2choose = ReadLine();
      int player2chooseInt;
+
+     // wybierz poprawną liczbę
      while(!Int32.TryParse(player2choose, out player2chooseInt)
      || player2chooseInt > 3
      || player2chooseInt <=0 ){
        WriteLine("Select correct number");
        player2choose = ReadLine();}
-     string player2chooseString = Console.ReadLine();
-     gamesRecord[gamesRecordCurrentIndex, 1] = player2chooseString;
+     gamesRecord[gamesRecordCurrentIndex, 1] = player2choose;
      Clear ();
 
      //rozpisać warunki wygranej
@@ -63,15 +66,22 @@ class MainClass {
      gamesRecord[gamesRecordCurrentIndex, 2] = "player2";*/
      
      gamesRecordCurrentIndex += 1;
-     WriteLine ("\n [push any key to continue or ctrl+C to exit]");
+     WriteLine ("\n Do you want to continue? \n [push any key to continue or (n + [enter]) to exit]");
+
+      } while (ReadLine() != "n");
       ReadKey ();
       Clear ();
+
+    WriteLine ("Score");
+    for (int i = 0; i < gamesRecordCurrentIndex; i++){
+    WriteLine ("Round #{0}: {1} - {2}, player1 wins {3}", i+1, gamesRecord[i,0], gamesRecord[i,1], gamesRecord[i,2]);
+    }
+  
       /*zrobić funkcje liczenia wyniku
       WriteLine ("Score:");
       WriteLine ("\n Player1\n Player2");
      ReadKey ();
      Clear ();*/
       
-    } 
   }
 }
