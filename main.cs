aea6eae2935e634aa5aa7069f63c460ef7b761fa
pajonkkk <ola.wolf363@wp.pl>
2020-04-05 16:58:45 +0000

    Zmiany po ostatnich zajęciach

--- main.cs
+++ main.cs
@@ -1,82 +1,93 @@
 using System;
 using static System.Console;
+public class MainClass{
+  public static void Main(string[] args) {
+    
+    string kolejnaGra;
+  
+  do{
+    string rozmiarGry;
+    int wielkoscGry = 0;
+        Console.WriteLine("Ile gier chcesz rozegrać?");
+        do {
+          rozmiarGry = Console.ReadLine();
+          if (!(int.TryParse(rozmiarGry, out _)))
+          {
+              Console.WriteLine("WPISZ LICZBE");
+          }
+        }while (!(int.TryParse(rozmiarGry, out wielkoscGry)));
 
-class MainClass {
-  public static void Main (string[] args) {
+    string[,] tablicaWynikow = new string [wielkoscGry, 3];
+    
+    //input gracza
+        for (int i = 0; i < wielkoscGry; i++){
+          string inputPlayerOne;
+          string inputPlayerTwo;
+                  WriteLine("Witaj graczu pierwszy! Wybierz:\n(k) Kamień\n(p) Papier\n(n) Nożyczki");
+                  do{
+                    inputPlayerOne = ReadLine();
+                      if(inputPlayerOne != "k" && inputPlayerOne != "p" && inputPlayerOne != "n"){
+                        Console.WriteLine("wcisnij klawisz k, p lub n, gnojcu");
+                      }
+                  }while (inputPlayerOne != "k" && inputPlayerOne != "p" && inputPlayerOne != "n");
+            WriteLine("Naciśnij cokolwiek");
+            
+            ReadKey();
+            Clear();
 
-    while(true){WriteLine("Witaj graczu pierwszy! Wybierz:\n(1) Kamień\n(2) Papier\n(3) Nożyczki");
-      string inputPlayerOne;
-      inputPlayerOne = ReadLine();
+                WriteLine("Witaj graczu drugi! Wybierz:\n(k) Kamień\n(p) Papier\n(n) Nożyczki");
+                  do{
+                    inputPlayerTwo = ReadLine();
+                      if(inputPlayerTwo != "k" && inputPlayerTwo != "p" && inputPlayerTwo != "n"){
+                        Console.WriteLine("wcisnij klawisz k, p lub n, pajacu");
+                      }
+                  }while (inputPlayerTwo != "k" && inputPlayerTwo != "p" && inputPlayerTwo != "n");
+            WriteLine("Naciśnij cokolwiek");
+            ReadKey();
+            Clear();
 
-      if(inputPlayerOne == "1"){
-        WriteLine("Wybrałeś kamień");
-      }
+        // porównywanie wynikow
+        if(inputPlayerOne == inputPlayerTwo){
+          WriteLine("remis :(");
+          
+          tablicaWynikow [i, 0] = inputPlayerOne;
+          tablicaWynikow [i, 1] = inputPlayerTwo;
+          tablicaWynikow [i, 2] = "remis";
 
-      else if(inputPlayerOne == "2"){
-        WriteLine("Wybrałeś papier");
-      }
-      
-      else if(inputPlayerOne == "3"){
-        WriteLine("Wybrałeś nożyce");
-      }
-
-      else{
-        WriteLine("Niepoprawny znak");
-      }
-
-      WriteLine("Naciśnij cokolwiek");
-      ReadKey();
-      Clear();
-
-      WriteLine("Dzień dobry graczu drugi, wybierz:\n(1) Kamień\n(2) Papier\n(3) Nożyczki");
-      string inputPlayerTwo;
-      inputPlayerTwo = ReadLine();
-
-      if(inputPlayerTwo == "1"){
-        WriteLine("Wybrałeś kamień, świetna decyzja");
-      }
-
-      else if(inputPlayerTwo == "2"){
-        WriteLine("Wybrałeś papier");
-      }
-      
-      else if(inputPlayerTwo == "3"){
-        WriteLine("Wybrałeś nożyce");
-      }
-
-      else{
-        WriteLine("Wpisz poprawną liczbę!");
-      }
-
-      if(inputPlayerOne == inputPlayerTwo){
-        WriteLine("remis :(");
-      }
-
-      else if((inputPlayerOne == "1" && inputPlayerTwo == "3")
-       || (inputPlayerOne == "2" && inputPlayerTwo == "1")
-       || (inputPlayerOne == "3" && inputPlayerTwo == "2")){
-          WriteLine("Zwycięzcą jest gracz 1!");
         }
 
-         else if((inputPlayerTwo == "1" && inputPlayerOne == "3")
-       || (inputPlayerTwo == "2" && inputPlayerOne == "1")
-       || (inputPlayerTwo == "3" && inputPlayerOne == "2")){
-          WriteLine("Gratulacje, graczu 2. Wygrałeś.");
-        }
+        else if((inputPlayerOne == "k" && inputPlayerTwo == "n")
+            || (inputPlayerOne == "p" && inputPlayerTwo == "k")
+             || (inputPlayerOne == "n" && inputPlayerTwo == "p")){
+            WriteLine("Zwycięzcą jest gracz 1!\n");
 
-      WriteLine("Czy chcesz wyjść? [y]");
-      string isExit = ReadLine();
+          tablicaWynikow [i, 0] = inputPlayerOne;
+          tablicaWynikow [i, 1] = inputPlayerTwo;
+          tablicaWynikow [i, 2] = "gracz 1";
+          }
 
-      if(isExit == "y"){
-        break;
-      }
-      Clear();
+          else if((inputPlayerTwo == "k" && inputPlayerOne == "n")
+        || (inputPlayerTwo == "p" && inputPlayerOne == "k")
+        || (inputPlayerTwo == "n" && inputPlayerOne == "p")){
+            WriteLine("Gratulacje, graczu 2. Wygrałeś.\n");
+
+          tablicaWynikow [i, 0] = inputPlayerOne;
+          tablicaWynikow [i, 1] = inputPlayerTwo;
+          tablicaWynikow [i, 2] = "gracz 2";
+          }
+    } // for i<10
+  //wyswietlanie tablicy
+  for (int i = 0; i < wielkoscGry; i++){
+    for (int j = 0; j < 3; j++){
+      System.Console.Write(tablicaWynikow[i,j] + "    ");
     }
-
-
-      
-
-      
-
+  System.Console.WriteLine();
   }
+
+   WriteLine("Czy chcesz powtórzyć? [y]");
+   kolejnaGra = Console.ReadLine();
+  }while(kolejnaGra == "y");
+
+
+  } 
 }
