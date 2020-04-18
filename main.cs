3329fa714d71d7f069c888914225cec7ff94feb5
JakubBaluszek <znajomioriginal@gmail.com>
2020-04-18 10:41:10 +0000

    Działa!

--- main.cs
+++ main.cs
@@ -15,18 +15,17 @@ string wybordrugiegogracza = ReadLine();
 if (wyborpierwszegogracza == wybordrugiegogracza){
   WriteLine ("Remis");
   gamesRecord[gamesRecordCurrentIndex, 2] = "nie przelala sie krew";}
-if (wyborpierwszegogracza == "1" && wybordrugiegogracza == "2")
-{WriteLine ("wygral Doktor Biceps");gamesRecord[gamesRecordCurrentIndex, 2] = "Doktor Biceps obronil swe wlosci";}
-if (wyborpierwszegogracza == "1" && wybordrugiegogracza == "3")
-{WriteLine ("wygral PlayerONE");gamesRecord[gamesRecordCurrentIndex, 2] = "playerone destroy the game";}
-if (wyborpierwszegogracza == "2" && wybordrugiegogracza == "1")
-{WriteLine ("wygral PlayerONE");gamesRecord[gamesRecordCurrentIndex, 2] = "playerone destroy the game";}
-if (wyborpierwszegogracza == "2" && wybordrugiegogracza == "3")
-{WriteLine ("wygral Doktor Biceps");gamesRecord[gamesRecordCurrentIndex, 2] = "Doktor Biceps obronil swe wlosci";}
-if (wyborpierwszegogracza == "3" && wybordrugiegogracza == "1")
-{WriteLine ("wygral Doktor Biceps");gamesRecord[gamesRecordCurrentIndex, 2] = "Doktor Biceps obronil swe wlosci";}
-if (wyborpierwszegogracza == "3" && wybordrugiegogracza == "2")
-{WriteLine ("wygral PlayerONE");gamesRecord[gamesRecordCurrentIndex, 2] = "playerone destroy the game";}
+if (
+  (wyborpierwszegogracza == "1" && wybordrugiegogracza == "2")||
+  (wyborpierwszegogracza == "2" && wybordrugiegogracza == "3")||
+  (wyborpierwszegogracza == "3" && wybordrugiegogracza == "1"))
+{WriteLine ("wygral Doktor Biceps");
+gamesRecord[gamesRecordCurrentIndex, 2] = "Doktor Biceps obronil swe wlosci";}
+if ((wyborpierwszegogracza == "1" && wybordrugiegogracza == "3")||
+    (wyborpierwszegogracza == "2" && wybordrugiegogracza == "1")||
+    (wyborpierwszegogracza == "3" && wybordrugiegogracza == "2"))
+{WriteLine ("wygral PlayerONE");gamesRecord[gamesRecordCurrentIndex, 2] = "playerone destroyed the game";}
+
 ReadKey();
 Clear();
 gamesRecord[gamesRecordCurrentIndex, 0] = wyborpierwszegogracza;
@@ -36,16 +35,17 @@ if (gamesRecordCurrentSize < gamesRecordSize){
   gamesRecordCurrentSize++;
 }
  Console.WriteLine ("Doktor Biceps, czy chcesz się znowu upokorzyć? Wpisz PB żeby walczyć o czarne złoto");
-} while (Console.ReadLine() != "PB");
-for (int i = 0; i < gamesRecordCurrentSize; i++){
-  int currentIndex;
-  if (gamesRecordCurrentSize < gamesRecordSize){
+} while (Console.ReadLine() == "PB");
+int currentIndex;
+if (gamesRecordCurrentSize < gamesRecordSize){
     currentIndex = 0;
   }
   else {
     currentIndex = gamesRecordCurrentIndex
   ;}
+for (int i = 0; i < gamesRecordCurrentSize; i++){
   Console.WriteLine ("Gra #{0}: {1} - {2}, wygrał gracz {3}",
     i+1, gamesRecord[currentIndex,0], gamesRecord[currentIndex,1], gamesRecord[currentIndex,2]);
+currentIndex = (currentIndex + 1) % gamesRecordSize; 
 }
 }}
