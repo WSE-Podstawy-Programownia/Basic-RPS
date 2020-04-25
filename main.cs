48ae623303a618d586d81bf405526f329f5f6f5c
krzysztofskalski7 <krzysztofskalski7@gmail.com>
2020-04-25 14:29:25 +0000

    Zrobione zadania 1a, 1b i 1c

--- main.cs
+++ main.cs
@@ -1,6 +1,81 @@
 using System;
 
 class MainClass {
+  
+
+  static void DisplayWelcomeMessage() {
+        
+        Console.WriteLine ("Welcome in RPS - Rock, Paper, Scissors");
+        Console.WriteLine ("Rules: Rock beats Scissors, Scissors beat Paper, Paper beats Rock");//zasady gry
+
+  }
+
+static string GetPlayerInput(){
+       
+       //deklaracja zmiennych funkcji
+        string rawInput;
+        string properInput = "";
+        
+        //instrukcja dla gracza
+        Console.WriteLine ("Choose (1)Rock, (2)Paper or (3)Scissors");
+        
+        // czytanie klawisza
+        rawInput = Console.ReadLine();
+        
+        //sprawdzanie poprawności inputu
+        while (rawInput != "1" && rawInput != "2" && rawInput != "3") {
+            Console.WriteLine ("Wrong input. Please enter correct one.\nChoose:\n[1] Rock\n[2] Paper\n[3] Scissors");
+            rawInput = Console.ReadLine();
+        }
+        if (rawInput == "1") { properInput = "Rock"; }
+        else if (rawInput == "2") { properInput = "Paper"; }
+        else { properInput = "Scissors"; }        
+        
+        return properInput;
+}
+
+static string DetermineWinner(string c_p1, string c_p2){
+ 
+ if (c_p1 == c_p2) {//wszystkie remisy
+              Console.WriteLine ("It's a tie");
+              return "Tie";
+              }
+            
+            else if (c_p1 == "Rock" && c_p2 == "Paper"){ // gracz 1 kamien, gracz 2 papier
+              Console.WriteLine ("Paper beats Rock. Player 2 wins.");
+              return "Player 2 won";
+              }
+                  
+            else if  (c_p1 == "Rock" && c_p2 == "Scissors") {// gracz 1 kamien, gracz 2 nozyce
+              Console.WriteLine ("Rock beats Scissors. Player 1 wins.");
+             return "Player 1 won";
+              }
+
+            else if (c_p1=="Paper" && c_p2 == "Rock"){ // gracz 1 papier, gracz 2 kamien
+              Console.WriteLine ("Paper beats Rock. Player 1 wins.");
+              return "Player 1 won";
+              }
+            
+            else if (c_p1=="Paper" && c_p2 == "Scissors"){ // gracz 1 papier, gracz 2 nożyce
+              Console.WriteLine ("Scissors beat Paper. Player 2 wins.");
+              return "Player 2 won";
+             }
+          
+            else if (c_p1=="Scissors" && c_p2 == "Rock"){ // gracz 1 nozyce gracz 2 kamien
+              Console.WriteLine ("Rock beats Scissors. Player 2 wins");
+              return "Player 2 won";
+              }
+            
+            else if (c_p1=="Scissors" && c_p2=="Paper") {//gracz 1 nozyce, gracz 2 papier
+              Console.WriteLine ("Scissors beat Paper. Player 1 wins.");
+              return "Player 1 won";
+              }
+
+              return "";
+            
+}
+  
+  
   public static void Main (string[] args) {
     
     string c_p1; //inicjalizacja zmiennej dla gracza 1
@@ -11,59 +86,23 @@ class MainClass {
     
     do {
     
-          Console.WriteLine ("Welcome in RPS - Rock, Paper, Scissors"); //wiadomość powitalna
-          Console.WriteLine ("Rules: Rock beats Scissors, Scissors beat Paper, Paper beats Rock");//zasady gry
-          Console.WriteLine ("Player 1: choose (1)Rock, (2)Paper or (3)Scissors");//instrukcja dla pierwszego gracza
-          c_p1=Console.ReadLine();//pierwszy gracz przypisuje wartość zmiennej
+          DisplayWelcomeMessage();
+                 
+          c_p1 = GetPlayerInput();//pierwszy gracz przypisuje wartość zmiennej
           gamesRecord[gamesRecordCurrentIndex, 0] = c_p1; //wpisanie wyboru pierwszego gracza do odpowiedniego miesjca w tablicy
-          Console.WriteLine ("Press any key to continue");//komunikat o końcu tury
-              Console.ReadKey(); //czekanie na klawisz
-          Console.Clear(); //czyszczenie ekranu
-          Console.WriteLine ("Player 2: choose (1)Rock, (2)Paper or (3)Scissors");//instrukcja dla drugiego gracza
-          c_p2=Console.ReadLine();//drugi gracz przypisuje wartość zmiennej
-          gamesRecord[gamesRecordCurrentIndex, 1] = c_p2;//wpisanie do tablicy wyboru gracza 2 
-          Console.WriteLine ("Press any key to continue");//komunikat o końcu tury
-          Console.ReadKey(); //czekanie na klawisz
-              Console.Clear(); //czyszczenie ekranu
-            
-            if (c_p1 == c_p2) {//wszystkie remisy
-              Console.WriteLine ("Tie");
-              gamesRecord[gamesRecordCurrentIndex, 2] = "Tie";
-              }
-            
-            else if (c_p1 == "1" && c_p2 == "2"){ // gracz 1 kamien, gracz 2 papier
-              Console.WriteLine ("Paper beats Rock. Player 2 wins.",c_p1,c_p2);
-              gamesRecord[gamesRecordCurrentIndex, 2] = "Player 2 won";
-              }
-                  
-            else if  (c_p1 == "1" && c_p2 == "3") {// gracz 1 kamien, gracz 2 nozyce
-              Console.WriteLine ("Rock beats Scissors. Player 1 wins.",c_p1,c_p2);
-              gamesRecord[gamesRecordCurrentIndex, 2] = "Player 1 won";
-              }
-
-            else if (c_p1=="2" && c_p2 == "1"){ // gracz 1 papier, gracz 2 kamien
-              Console.WriteLine ("Paper beats Rock. Player 1 wins.",c_p1, c_p2);
-              gamesRecord[gamesRecordCurrentIndex, 2] = "Player 1 won";
-              }
-            
-            else if (c_p1=="2" && c_p2 == "3"){ // gracz 1 papier, gracz 2 nożyce
-              Console.WriteLine ("Scissors beat Paper. Player 2 wins.",c_p1,c_p2);
-              gamesRecord[gamesRecordCurrentIndex, 2] = "Player 2 won";
-             }
           
-            else if (c_p1=="3" && c_p2 == "1"){ // gracz 1 nozyce gracz 2 kamien
-              Console.WriteLine ("Rock beats Scissors. Player 2 wins.",c_p1,c_p2);
-              gamesRecord[gamesRecordCurrentIndex, 2] = "Player 2 won";
-              }
-            
-            else if (c_p1=="3" && c_p2=="2") {//gracz 1 nozyce, gracz 2 papier
-              Console.WriteLine ("Scissors beat Paper. Player 1 wins.", c_p1,c_p2);
-              gamesRecord[gamesRecordCurrentIndex, 2] = "Player 1 won";
-              }
+        
+          Console.Clear(); //czyszczenie ekranu
+          
+          c_p2=GetPlayerInput();//drugi gracz przypisuje wartość zmiennej
+          gamesRecord[gamesRecordCurrentIndex, 1] = c_p2;//wpisanie do tablicy wyboru gracza 2 
+          
+          
+              Console.Clear(); //czyszczenie ekranu
+              gamesRecord[gamesRecordCurrentIndex,2]= DetermineWinner(c_p1, c_p2);
+                     
               gamesRecordCurrentIndex = (gamesRecordCurrentIndex + 1) % gamesRecordSize;
               Console.WriteLine ("Do you want to quit? (t)");
-              
-
 
     } while (Console.ReadLine() != "t");
 
