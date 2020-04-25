904aaddcb322702120ea57c00e11e70bfba395c8
kungwho <ostrowski.alexander@gmail.com>
2020-04-25 14:04:45 +0000

    2c

--- main.cs
+++ main.cs
@@ -3,6 +3,12 @@ using static System.Console;
 
 class MainClass {
 
+static int gamesRecordSize = 10;
+static string[,] gamesRecord = new string[gamesRecordSize,3];
+static int gamesRecordCurrentIndex = 0;
+static int gamesRecordCurrentSize = 0;
+
+
   static void DisplayWelcomeMessage (){
    WriteLine ("Welcome to a simple Rock-Paper-Scissors game. \nThe rules are very simple - each player chooses Rock, Paper or Scissors choice by entering the choice's number\n[1] Rock\n[2] Paper\n[3] Scissors\nand confirm it by clicking Enter.\nAfter both player choose, the winner is determined. After each game the application will ask the players if they want to continue, and if the player repond with anything else than [y]es than the game finishes and presents the record of the last up to 10 games.\n\nHave fun!\n(click any key to continue)");
 }
@@ -56,60 +62,53 @@ for (int i = 0; i < gamesRecordCurrentSize; i++){
   WriteLine ("Game #{0}:\t{1}\t-\t{2},\t{3}", i+1, gamesRecord[currentIndex,0], gamesRecord[currentIndex,1], gamesRecord[currentIndex,2]);
   currentIndex = (currentIndex + 1) % gamesRecordCurrentSize;
 }
+}
 
+static void PlayGame (){
+Clear();
+string firstPlayerChoiceString = GetPlayerInput();
+gamesRecord[gamesRecordCurrentIndex, 0] = firstPlayerChoiceString;
+Clear ();
+string secondPlayerChoiceString = GetPlayerInput();
+gamesRecord[gamesRecordCurrentIndex, 1] = secondPlayerChoiceString;
+Clear ();
+gamesRecord[gamesRecordCurrentIndex, 2] = DetermineWinner(firstPlayerChoiceString, secondPlayerChoiceString);
+gamesRecordCurrentIndex = (gamesRecordCurrentIndex + 1) % gamesRecordSize;
+if (gamesRecordCurrentSize < gamesRecordSize){
+  gamesRecordCurrentSize++;
+}
+WriteLine("Do you want to play another round? [y]");
+if (ReadKey(true).Key == ConsoleKey.Y){
+  PlayGame();
+}
 }
 
 
 
+static void MainMenuLoop (){
+  ConsoleKeyInfo inputKey;
+do {
+  Clear();
+  WriteLine ("Rock-Paper-Scissors Menu:\n\t[1] Play a game\n\t[2] Show rules\n\t[3] Display last games' record\n\t[ESC] Exit");
+  inputKey = ReadKey(true);
+if (inputKey.Key == ConsoleKey.D1){
+  PlayGame();
+}
+else if (inputKey.Key == ConsoleKey.D2){
+  DisplayWelcomeMessage();
+}
+else if (inputKey.Key == ConsoleKey.D3){
+  DisplayGamesHistory (gamesRecord, gamesRecordSize, gamesRecordCurrentSize, gamesRecordCurrentIndex);
+}
+else { continue; }
+WriteLine ("(click any key to continue)");
+ReadKey(true);
+} while (inputKey.Key != ConsoleKey.Escape);
 
+}
 
-  public static void Main (string[] args) {
-    // declaration and initialization of the global game variables
-    int gamesRecordSize = 10;
-    string[,] gamesRecord = new string[gamesRecordSize,3];
-    int gamesRecordCurrentIndex = 0;
-    int gamesRecordCurrentSize = 0;
-    
-   DisplayWelcomeMessage();
-
-    // Use the ReadKey() method to get any key as input
-    ReadKey();
-
-    do {
-      // Clear the console before the round
-      Clear();
-
-      // FirstPlayer makes his choice with data validation
-     string firstPlayerChoiceString = GetPlayerInput();
-     gamesRecord[gamesRecordCurrentIndex, 0] = firstPlayerChoiceString;
-
-      
-      // Clear the console so the SecondPlayer doesn't see what the FirstPlayer chose
-      Clear ();
-      
-      // SecondPlayer makes his choice with data validation
-     string secondPlayerChoiceString = GetPlayerInput();
-     gamesRecord[gamesRecordCurrentIndex, 0] = secondPlayerChoiceString;
-      
-      // Clear the console before announcing the winner
-      Clear ();
-
-      // Check the result
-gamesRecord[gamesRecordCurrentIndex, 2] = DetermineWinner(firstPlayerChoiceString, secondPlayerChoiceString);
-
-
-      // Increment the games index counter and current history size
-      gamesRecordCurrentIndex = (gamesRecordCurrentIndex + 1) % gamesRecordSize;
-      if (gamesRecordCurrentSize < gamesRecordSize){
-        gamesRecordCurrentSize++;
-      }
-
-      // Ask the players if they want to continue
-      WriteLine("Do you want to player another round? [y]");
-    } while (ReadLine() == "y");
-
-    // Present the games' history
-DisplayGamesHistory (gamesRecord, gamesRecordSize, gamesRecordCurrentSize, gamesRecordCurrentIndex);
+public static void Main (string[] args) {
+MainMenuLoop();
     
   }
 }
