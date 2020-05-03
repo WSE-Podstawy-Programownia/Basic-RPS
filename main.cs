155369fef3e6fef6c3b7c9c588c2f80f9659060a
krzysztofskalski7 <krzysztofskalski7@gmail.com>
2020-05-03 15:41:50 +0000

    Zrobione zadania 2a, 2b i 2c

--- main.cs
+++ main.cs
@@ -2,16 +2,23 @@ using System;
 
 class MainClass {
   
+static int gamesRecordSize = 10;
+static string[,] gamesRecord = new string[gamesRecordSize,3];
+static int gamesRecordCurrentIndex = 0;
+static int gamesRecordCurrentSize = 0;
+
+
 
   static void DisplayWelcomeMessage() {
-        
+        //funkcja wyświetlająca wiadomosc powitalna/reguły gry
         Console.WriteLine ("Welcome in RPS - Rock, Paper, Scissors");
         Console.WriteLine ("Rules: Rock beats Scissors, Scissors beat Paper, Paper beats Rock");//zasady gry
 
   }
 
 static string GetPlayerInput(){
-       
+       //funkcja pobierajaca input gracza
+
        //deklaracja zmiennych funkcji
         string rawInput;
         string properInput = "";
@@ -35,7 +42,9 @@ static string GetPlayerInput(){
 }
 
 static string DetermineWinner(string c_p1, string c_p2){
- 
+        //funkcja określajaca kto wygral rozgrywke
+
+
  if (c_p1 == c_p2) {//wszystkie remisy
               Console.WriteLine ("It's a tie");
               return "Tie";
@@ -75,14 +84,20 @@ static string DetermineWinner(string c_p1, string c_p2){
             
 }
 
-static void DisplayGamesHistory (string[,] gamesRecord, int gamesRecordSize, int gamesRecordCurrentSize = 10, int lastRecordIndex = 0){
-int currentIndex;
-if (gamesRecordCurrentSize < gamesRecordSize){
-  currentIndex = 0;
-}
-else {
-  currentIndex = lastRecordIndex;
-  }
+static void DisplayGamesHistory (string[,] gamesRecord, int gamesRecordSize, int gamesRecordCurrentSize = 10, int lastRecordIndex = 0)
+{  // funkcja wyświetlajaca wyniki ostatnich gier
+
+
+  int currentIndex;
+    if (gamesRecordCurrentSize < gamesRecordSize){
+              
+              currentIndex = 0;
+    }
+    else {
+
+          currentIndex = lastRecordIndex;
+      
+          }
 
  Console.WriteLine ("The scores are:");
    for (int i = 0; i < gamesRecordCurrentSize; i++){
@@ -92,13 +107,83 @@ else {
 
 }
 
+static void MainMenuLoop (){
+
+      // funkcja odpowiedzialna za pętle glownego menu
+
+  ConsoleKeyInfo inputKey;
+   
+
+  do {
+     Console.Clear();
+    
+     Console.WriteLine ("Rock-Paper-Scissors Menu:\n\t[1] Play a game\n\t[2] Show rules\n\t[3] Display last games' record\n\t[ESC] Exit");
+       
+      inputKey = Console.ReadKey(true);
+
+      if (inputKey.Key == ConsoleKey.D1){
+            PlayGame();
+        }
+
+      else if (inputKey.Key == ConsoleKey.D2){
+            DisplayWelcomeMessage();
+        }
+      else if (inputKey.Key == ConsoleKey.D3){
+            DisplayGamesHistory (gamesRecord, gamesRecordSize, gamesRecordCurrentSize, gamesRecordCurrentIndex);
+
+      }
+      else { continue; }
+          Console.WriteLine ("(Press any key to continue)");
+            Console.ReadKey(true);
+
+
+    
+    } 
+  while (inputKey.Key != ConsoleKey.Escape);
+
+}
+
+static void PlayGame (){
+          
+          Console.Clear(); //czyszczenie ekranu
+
+          string c_p1 = GetPlayerInput();//pierwszy gracz przypisuje wartość zmiennej
+          gamesRecord[gamesRecordCurrentIndex, 0] = c_p1; //wpisanie wyboru pierwszego gracza do odpowiedniego miesjca w tablicy
+                
+          Console.Clear(); //czyszczenie ekranu
+          
+          string c_p2=GetPlayerInput();//drugi gracz przypisuje wartość zmiennej
+          gamesRecord[gamesRecordCurrentIndex, 1] = c_p2;//wpisanie do tablicy wyboru gracza 2 
+         
+          Console.Clear(); //czyszczenie ekranu
+              
+          gamesRecord[gamesRecordCurrentIndex,2]= DetermineWinner(c_p1, c_p2);
+                 
+          gamesRecordCurrentIndex = (gamesRecordCurrentIndex + 1) % gamesRecordSize;
+          
+          if (gamesRecordCurrentSize < gamesRecordSize){
+                  
+                  gamesRecordCurrentSize++;
+              
+              }
+
+          Console.WriteLine("Do you want to play another round? [y]");
+              
+              if (Console.ReadKey(true).Key == ConsoleKey.Y){
+                
+                PlayGame();
+                
+                }
+
+}
+
   
   
   public static void Main (string[] args) {
     
-    string c_p1; //inicjalizacja zmiennej dla gracza 1
-    string c_p2; //inicjalizacja zmiennej dla gracza 2
-    int gamesRecordSize = 10;
+    //string c_p1; //inicjalizacja zmiennej dla gracza 1
+    //string c_p2; //inicjalizacja zmiennej dla gracza 2
+    /*int gamesRecordSize = 10;
     string[,] gamesRecord = new string[gamesRecordSize,3]; //inicjalizacja tablicy wyników
     int gamesRecordCurrentIndex = 0;
     int gamesRecordCurrentSize = 10;
@@ -125,7 +210,9 @@ else {
 
     } while (Console.ReadLine() != "t");
 
-       DisplayGamesHistory (gamesRecord, gamesRecordSize, gamesRecordCurrentSize, gamesRecordCurrentIndex);
+       DisplayGamesHistory (gamesRecord, gamesRecordSize, gamesRecordCurrentSize, gamesRecordCurrentIndex); */
+
+       MainMenuLoop();
 
 
 }
