0c3e362f84424d0757e50446e25d7e0e84c8f916
kungwho <ostrowski.alexander@gmail.com>
2020-05-30 13:41:13 +0000

    3a

---
+++ game.cs
@@ -0,0 +1,16 @@
+using System;
+using static System.Console;
+
+public class Game {
+  Player playerOne, playerTwo;
+  GamesRecord gamesRecord;
+
+public Game () {
+  playerOne = new Player ();
+  playerTwo = new Player ();
+  gamesRecord = new GamesRecord ();
+}
+
+
+}
+
