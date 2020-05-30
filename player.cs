c71cddeaa6c40ae4018bb86bdaff9597e9105f29
Ch0udini <kakollukia@gmail.com>
2020-05-30 16:03:44 +0000

    zadanie 1

--- player.cs
+++ player.cs
@@ -1,5 +1,8 @@
+using System;
+using static System.Console;
+
 class Player {
-  public string playerName;
+ public string playerName;
    public Player (string playerName) {
    this.playerName = playerName;
   }
