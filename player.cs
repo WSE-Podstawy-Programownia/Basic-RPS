edd198d0e72998ecf43559454c8068a6465e0112
Karina Ryabchenko <karina.r.1999@gmail.com>
2020-06-13 16:17:26 +0200

    zajęcia # 6

--- player.cs
+++ player.cs
@@ -1,22 +1,26 @@
 using System;
 using static System.Console;
 
-class Player {
-  public string playerName;
-  public Player (string playerName) {
-   this.playerName = playerName;
-   }
-  public void SetPlayerName () {
-    do
+class Player
+{
+    public string playerName;
+    public Player(string playerName)
     {
-      Clear();
-      Write("Please enter player name: ");
-      playerName = ReadLine();
+        this.playerName = playerName;
     }
-    while(playerName.Length == 0 || playerName.Length > 10);
-  }
-  public Player () {
-   SetPlayerName();
-  }
- 
+    public void SetPlayerName()
+    {
+        do
+        {
+            Clear();
+            Write("Please enter player name: ");
+            playerName = ReadLine();
+        }
+        while (playerName.Length == 0 || playerName.Length > 10);
+    }
+    public Player()
+    {
+        SetPlayerName();
+    }
+
 }
