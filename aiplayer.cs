d1c17681b6d793cdb151d7e46146fe2a5405ca2a
Karolina <kkpiwowarczyk@gmail.com>
2020-06-27 16:01:14 +0200

    Zadanie dodatkowe 2 - zrobione

--- aiplayer.cs
+++ aiplayer.cs
@@ -4,12 +4,17 @@ using System.Linq;
 
 class AIPlayer : Player
 {
-    Random random;
+    private static Random random = new Random();
 
-    public AIPlayer() : base("[AI Player]")
-    {        
-        random = new Random();
-    }
+    private static string[] names = new string[]
+    {
+        "Bulbasaur", "Ivysaur", "Venusaur", "Charmander", "Charmeleon", "Charizard", "Squirtle", "Wartortle", "Blastoise", "Caterpie", "Metapod", "Butterfree", "Pikachu"
+    };
+
+    public AIPlayer() 
+        : base(names[random.Next(0, names.Length)])
+    {                
+    }    
 
     override public void GetInput(Dictionary<string, string> inputTable)
     {
