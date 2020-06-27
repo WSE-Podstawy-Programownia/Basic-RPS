1387203898aa5b9d62f5f2a2f5228e3f388eb260
Agnieszka <terrorystka.aga@gmail.com>
2020-06-27 18:32:57 +0200

    zadanie 1b - lab7

---
+++ gamecontroller.cs
@@ -0,0 +1,49 @@
+using System;
+class GameController {
+    Game game;
+    GamesRecord gamesRecord;
+    public GameController () {
+      gamesRecord = new GamesRecord();
+    }
+
+    public void DisplayRules (bool withWelcomeMessage = true) {
+        if (withWelcomeMessage) {
+            WriteLine ("Welcome to a simple Rock-Paper-Scissors game!");
+    }
+
+    WriteLine ("The rules are very simple - each player chooses Rock, Paper or Scissors choice by entering the choice's number\n[1] Rock\n[2] Paper\n[3] Scissors\nand confirm it by clicking Enter.\nAfter both player choose, the winner is determined. After each game the application will ask the players if they want to continue, and if the player repond with anything else than [y]es than the game finishes and presents the record of the last up to 10 games.\n\nHave fun!");
+    }
+
+    
+    public void MainMenuLoop (){
+        
+        // funkcja glownej petli 
+
+        ConsoleKeyInfo inputKey;
+
+        do {
+            Clear();
+            WriteLine ("Rock-Paper-Scissors Menu:\n\t[1] Play a game\n\t[2] Show rules\n\t[3] Display last games' record\n\t[ESC] Exit");
+            inputKey = ReadKey(true);
+
+            if (inputKey.Key == ConsoleKey.D1){
+                //po wybraniu Play tworzyła nowy obiekt Game i na nim wywoływała metodę Play 1b-48
+                game = new Game();
+                game.Play();  
+            }
+
+            else if (inputKey.Key == ConsoleKey.D2){
+                DisplayRules(false);
+            }
+
+            else if (inputKey.Key == ConsoleKey.D3){
+                gamesRecord.DisplayGamesHistory();
+        }
+
+            else { continue; }
+            WriteLine ("(click any key to continue)");
+            ReadKey(true);
+            } 
+        while (inputKey.Key != ConsoleKey.Escape);
+    }
+}
--- main.cs
+++ main.cs
@@ -2,14 +2,21 @@ using System;
 using static System.Console;
 
 class MainClass {
-
+//modyfikować funkcję Main, aby powoływała nowy obiekt tej klasy (zamiast poprzednio stosowanego obiektu Game) i uruchomić MainMenuLoop
+//1b-49
   public static void Main (string[] args) {
-    // MainMenuLoop();
-    // Player playerOne = new Player("Player One");
-    // WriteLine(playerOne.playerName);
-    // Player playerTwo = new Player();
-    // WriteLine(playerTwo.playerName);
-    // GamesRecord gamesRecord = new GamesRecord();
-    Game game = new Game();
+  GameController gameController = new GameController();
+  gameController.MainMenuLoop();
   }
 }
+
+
+  // public static void Main (string[] args) {
+  //   // MainMenuLoop();
+  //   // Player playerOne = new Player("Player One");
+  //   // WriteLine(playerOne.playerName);
+  //   // Player playerTwo = new Player();
+  //   // WriteLine(playerTwo.playerName);
+  //   // GamesRecord gamesRecord = new GamesRecord();
+  //   Game game = new Game();
+  // }
