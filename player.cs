4aa71cd5434889b42d1c7a7bf467dfaeaf091799
kungwho <ostrowski.alexander@gmail.com>
2020-06-27 13:36:38 +0000

    2b

--- player.cs
+++ player.cs
@@ -1,8 +1,10 @@
 using System;
 using static System.Console;
+using System.Collections.Generic;
 
 public class Player {
   public string playerName;
+  public string lastInput;
 
  public Player (string playerName) {
    this.playerName = playerName;
@@ -16,6 +18,20 @@ public void SetPlayerName () {
   Write("Please enter player name: ");
   playerName = ReadLine();
 }
+
+public void GetInput (Dictionary<string, string> inputTable) {
+  string rawInput;
+  WriteLine ("{0}, Choose:", playerName);
+  foreach(KeyValuePair<string, string> entry in inputTable) {
+    WriteLine ("[{0}] {1}", entry.Key, entry.Value);
+  }
+  rawInput = ReadLine();
+  while (!inputTable.TryGetValue(rawInput, out lastInput)) {
+    WriteLine ("Wrong input. Please enter correct one.");
+    rawInput = ReadLine();
+  }
+}
+
 
 
 }
