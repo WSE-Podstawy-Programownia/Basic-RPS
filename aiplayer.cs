50da57b48e20ffb06306001be413a4dbd9243f66
Karolina <kkpiwowarczyk@gmail.com>
2020-06-27 15:31:29 +0200

    Zadanie 3a - zrobione

---
+++ aiplayer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+class AIPlayer : Player
+{
+    Random random;
+
+    public AIPlayer()
+    {
+        this.playerName += " [AI Player]";
+        random = new Random();
+    }
+}
