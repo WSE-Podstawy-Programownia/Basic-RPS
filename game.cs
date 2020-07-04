c8d056101dd9120fdb827ef85fe51df3306ab585
Karolina <kkpiwowarczyk@gmail.com>
2020-07-04 12:00:48 +0200

    Zadanie 2b - zrobione

---
+++ game.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+abstract class Game
+{
+    protected Player playerOne, playerTwo;
+    protected Dictionary<string, string> inputTable;
+    public GamesRecord gamesRecord;
+    public abstract string GetPlayerInput(Player player);
+    public abstract void Play();
+}
