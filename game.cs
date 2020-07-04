020633479fb5d92b2d7990d3bfc1ffc82568a441
Agnieszka <terrorystka.aga@gmail.com>
2020-07-04 17:19:34 +0200

    lab8 - 2d

--- game.cs
+++ game.cs
@@ -1,9 +1,12 @@
-using System;
 using System.Collections.Generic;
 
 abstract class Game {
+    private static string gameName;
+    private static string gameRules;
     protected Player playerOne, playerTwo;
     protected Dictionary<string, string> inputTable;
+    public string GameName { get => gameName; set => gameName = value; }
+    public string GameRules { get => gameRules; set => gameRules = value; }
     public GamesRecord gamesRecord;
     public abstract void Play ();
 }
