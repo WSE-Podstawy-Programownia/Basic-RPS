140dd516f393b39b8bf4b638dfa918db47730bde
kungwho <ostrowski.alexander@gmail.com>
2020-07-04 09:34:09 +0000

    1d

--- aiplayer.cs
+++ aiplayer.cs
@@ -5,10 +5,11 @@ using System.Linq;
 class AIPlayer : Player {
   Random random;
 
-  public AIPlayer () {
-      this.playerName += " [AI Player]";
+  public AIPlayer () : base(false) {
+      this.playerName = "AI Player";
       random = new Random();
   }
+
 
 override public void GetInput (Dictionary<string, string> inputTable) {
   lastInput = inputTable.ElementAt(random.Next(inputTable.Count)).Value;
