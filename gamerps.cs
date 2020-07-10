899aa8e3b148f434301f68d6e6b7492e2b76342b
Agnieszka <terrorystka.aga@gmail.com>
2020-07-10 20:40:04 +0200

    usuniecie starych komentarzy po lab#8

--- gamerps.cs
+++ gamerps.cs
@@ -3,16 +3,6 @@ using static System.Console;
 using System.Collections.Generic;
 
 class GameRPS : Game {
-//   Player playerOne, playerTwo;
-//    //dodawanie slownika
-//  Dictionary<string, string> inputTable = new Dictionary<string, string> () 
-//     {
-//       {"1", "Rock"},
-//       {"2", "Paper"},
-//       {"3", "Scissors"}
-//     };
-//   public GamesRecord gamesRecord;
-
   public GameRPS (bool singleplayer = false) {
     GameName = "Rock-Paper-Scissors";
     GameRules = "The rules are very simple - each player chooses Rock, Paper or Scissors choice by entering the choice's number\n[1] Rock\n[2] Paper\n[3] Scissors\nand confirm it by clicking Enter.\nAfter both player choose, the winner is determined. After each game the application will ask the players if they want to continue, and if the player repond with anything else than [y]es than the game finishes and presents the record of the last up to 10 games.\n\nHave fun!";
@@ -87,31 +77,4 @@ class GameRPS : Game {
       Play();
     }
   }
-
-  // public void MainMenuLoop (){
-  //   ConsoleKeyInfo inputKey;
-  //   do {
-  //     Clear();
-  //     WriteLine ("Rock-Paper-Scissors Menu:\n\t[1] Play a game\n\t[2] Show rules\n\t[3] Display last games' record\n\t[ESC] Exit");
-  //     inputKey = ReadKey(true);
-
-  //     if (inputKey.Key == ConsoleKey.D1){
-  //       //po wybraniu Play tworzyła nowy obiekt Game i na nim wywoływała metodę Play 1b
-  //       game = new Game();
-  //       game.Play();  
-  //     }
-
-  //     else if (inputKey.Key == ConsoleKey.D2){
-  //       DisplayRules(false);
-  //     }
-
-  //     else if (inputKey.Key == ConsoleKey.D3){
-  //       gamesRecord.DisplayGamesHistory();
-  //     }
-
-  //     else { continue; }
-  //     WriteLine ("(click any key to continue)");
-  //     ReadKey(true);
-  //   } while (inputKey.Key != ConsoleKey.Escape);
-  // }
 }
