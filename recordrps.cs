56546b9e192b53ded92a3191efa452944c428423
niedobitek <a8mamptpgd@gmail.com>
2020-07-11 13:32:06 +0200

    Zajęcia #9. Zad 1B

---
+++ recordrps.cs
@@ -0,0 +1,18 @@
+class RecordRPS : IRecord
+{
+    string playerOneChoice;
+    string playerTwoChoice;
+    string result;
+
+    public RecordRPS (string playerOneChoice, string playerTwoChoice, string result)
+    {
+        this.playerOneChoice = playerOneChoice;
+        this.playerTwoChoice = playerTwoChoice;
+        this.result = result;
+    }
+
+    override public string ToString() 
+    {
+         return string.Format("{0,20} : {1, -20}", playerOneChoice + " vs " + playerTwoChoice, result);
+    }
+}
