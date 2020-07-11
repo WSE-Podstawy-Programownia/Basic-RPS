4f76464bd7ef2e577f47a5055b42739acf1d2208
kungwho <ostrowski.alexander@gmail.com>
2020-07-11 08:45:32 +0000

    1c

--- gamerps.cs
+++ gamerps.cs
@@ -66,7 +66,7 @@ public override void Play () {
   Clear ();
 
   string gameResult = DetermineWinner(playerOne, playerTwo);
-  gamesRecord.AddRecord(playerOne.LastInput, playerTwo.LastInput, gameResult);
+  gamesRecord.AddRecord(new RecordRPS(playerOne.LastInput, playerTwo.LastInput, gameResult));
 
 
 WriteLine("Do you want to play another round? [y]");
--- irecord.cs
+++ irecord.cs
@@ -1,3 +1,3 @@
-interface IRecord {
+public interface IRecord {
   string ToString ();
 }
