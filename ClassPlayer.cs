36523294244047e5202352676e73b9eef2b78666
MarcinWSE <marcinmarchewka11@gmail.com>
2020-07-16 20:04:02 +0200

    usuniecie bledu stackoverflow

--- ClassPlayer.cs
+++ ClassPlayer.cs
@@ -1,50 +1,55 @@
 using System;
-using static System.Console;
 using System.Collections.Generic;
 using System.Linq;
-
+using static System.Console;
 class Player {
-  protected string playerName;
+protected string playerName;
     private string lastInput;
 
     public string PlayerName {
-    get {
-      return PlayerName;
-    }
-    set {
-      PlayerName = value;
-    }
+  get {
+  return playerName;
   }
+  set {
+  playerName = value;
+}
+}
 
     public string LastInput { get => lastInput; set => lastInput = value; }
 
-    public Player (string playerName) {
-   this.PlayerName = playerName;
-  }
-  public void SetPlayerName () {
-  Write("Please enter player name: ");
-  PlayerName = ReadLine();
-}
-public Player (bool invokeNameInput = true) {
-  if (invokeNameInput) {
+    public Player (bool invokeNameInput = true) {
+    if (invokeNameInput) {
     SetPlayerName();
+    };
   }
-}
-//Błąd StackOverflow
 
-
-virtual public void GetInput (Dictionary<string, string> inputTable) {
-  string rawInput;
-  WriteLine ("{0}, Choose:", playerName);
-  foreach(KeyValuePair<string, string> entry in inputTable) {
-    WriteLine ("[{0}] {1}", entry.Key, entry.Value);
+public void SetPlayerName () {
+  Clear();
+  Write("Please enter players name: ");
+  playerName = ReadLine();
   }
-  rawInput = ReadLine();
-  while (!inputTable.TryGetValue(rawInput, out lastInput)) {
-    WriteLine ("Wrong input. Please enter correct one.");
+  public Player () {
+  SetPlayerName();
+  }
+  virtual public void GetInput (Dictionary<string, string> inputTable) {
+    string rawInput;
+    WriteLine ("{0}, Wybierz:", playerName);
+    foreach(KeyValuePair<string, string> entry in inputTable) {
+      WriteLine ("[{0}] {1}", entry.Key, entry.Value);
+    }
     rawInput = ReadLine();
+    while (!inputTable.TryGetValue(rawInput, out lastInput)) {
+      WriteLine ("Wrong input. Please enter correct one.");
+      rawInput = ReadLine();
+    }
   }
-}
+
+
+
+  
+
+
+
 
 
 }
