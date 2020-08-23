916053c7e1fc64aa0d68932eb1cdde859ef1d7ce
pprzybyla <patprzyb00@gmail.com>
2020-08-23 19:55:07 +0000

    Lab_4_zad_2b

--- main.cs
+++ main.cs
@@ -9,6 +9,7 @@ public class Program
   public static void Powitanie ()
   {
     Console.WriteLine("Cześć! Zagrajmy w RPS.");
+    Console.WriteLine("Zasady są proste - Kamień bije Nożyce, Nożyce biją Papier, Papier bije Kamień. Wybierz figurę, która pokona figurę wybraną przez Twojego przeciwnika, żeby zyskać punkt.");
   }
   
   public static string GraczWybiera ()
@@ -57,81 +58,30 @@ public class Program
      return 2;
    }
   }
+
+static void MenuGlowne ()
+{
+  ConsoleKeyInfo inputKey;
+do 
+{
+  Clear();
+  WriteLine ("Kamień, Papier i Nożyce - Menu:\n\t[1] Zagraj\n\t[2] Zasady\n\t[ESC] Wyjście");
+  inputKey = ReadKey(true);
+  
+  if (inputKey.Key == ConsoleKey.D1){
+  WriteLine("Opcja pierwsza");
+  }
+  else if (inputKey.Key == ConsoleKey.D2){
+  Powitanie();
+  }
+  else { continue; }
+WriteLine ("(Wybierz jedną z opcji)");
+ReadKey(true);
+} while (inputKey.Key != ConsoleKey.Escape);
+}
+
   
   public static void Main(string[] args) 
   {
-    string odp = "";
-    int wynikGracz = 0;
-    int wynikPC = 0;
-
-    Powitanie();
-
-    while (odp != "NIE") //Główna pętla z grą
-    {
-      Console.WriteLine("Wybierz jedno: \n1->Kamień\n2->Papier\n3->Nożyce");
-      string[] choices = new string[3] { "Kamień", "Papier", "Nożyce" };
-      Random rnd = new Random();
-      int n = rnd.Next(0, 3); //Komputer losuje
-      Console.WriteLine("Twój wybór:");
-      string user = Console.ReadLine().ToUpper();
-      Console.WriteLine("Komputer:" + choices[n]);
-      int wynik = SprawdzenieZwyciezcy(user, choices[n]);
-      if(wynik == 1)
-      {
-        Console.WriteLine("Wygrałeś!");
-        wynikGracz += 1;
-      }
-      else if(wynik == -1)
-      {
-        Console.WriteLine("Przegrałeś!");
-        wynikPC += 1; //+1 do wyniku komputera
-      }
-      else if(wynik == 0)
-      {
-         Console.WriteLine("Remis!");
-      }
-      
-      if (user == "1" && choices[n] == "Nożyce")
-      {
-      Console.WriteLine("Wygrałeś!");
-      wynikGracz += 1; //+1 do wyniku gracza
-      }
-      else if (user == "1" && choices[n] == "Papier")
-      {
-      Console.WriteLine("Przegrałeś!");
-      wynikPC += 1; //+1 do wyniku komputera
-      }
-      else if (user == "2" && choices[n] == "Kamień")
-      {
-      Console.WriteLine("Wygrałeś!");
-      wynikGracz += 1;
-      }
-      else if (user == "2" && choices[n] == "Nożyce")
-      {
-      Console.WriteLine("Przegrałeś!");
-      wynikPC += 1;
-      }
-      else if (user == "3" && choices[n] == "Kamień")
-      {
-      Console.WriteLine("Przegrałeś!");
-      wynikPC += 1;
-      }
-      else if (user == "3" && choices[n] == "Papier")
-      {
-      Console.WriteLine("Wygrałeś!");
-      wynikGracz += 1;
-      }
-      else
-      {
-
-      }
-      Console.WriteLine("Kontynuujesz? (TAK/NIE):");
-      odp = Console.ReadLine().ToUpper();
-      Console.WriteLine("---------------------------------------");
-    }
-    Console.WriteLine("Koniec gry!");
-    Console.WriteLine("Gracz: " + wynikGracz);
-    Console.WriteLine("Komputer: " + wynikPC);
-  }
-}
-
+    MenuGlowne();
+  } 
