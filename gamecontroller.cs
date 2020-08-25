d741ff847bbdd6b448907276e95a14811d27255f
pprzybyla <patprzyb00@gmail.com>
2020-08-25 15:51:07 +0000

    Lab7_zad4b

--- gamecontroller.cs
+++ gamecontroller.cs
@@ -29,7 +29,7 @@ public GameController ()
       // Clear the console
       Clear();
       // Write the menu message
-      WriteLine ("Rock-Paper-Scissors Menu:\n\t[1] Play a game\n\t[2] Show rules\n\t[3] Display last games' record\n\t[ESC] Exit");
+      WriteLine ("Rock-Paper-Scissors Menu:\n[1] Player vs Player\n[2] Player vs AI\n[3] Show rules\n[4] Display last games' record\n[ESC] Exit");
       inputKey = ReadKey(true);
       if (inputKey.Key == ConsoleKey.D1){
         game = new Game();
@@ -39,11 +39,17 @@ public GameController ()
 
       }
       else if (inputKey.Key == ConsoleKey.D2){
-        DisplayRules(false); // false to not display the "Welcome.." part
+          game = new Game(true);
+          game.Play();
+          gamesRecord += game.gamesRecord;
+
       }
       else if (inputKey.Key == ConsoleKey.D3){
-        gamesRecord.DisplayGamesHistory();
+          DisplayRules(false);
       }
+       else if (inputKey.Key == ConsoleKey.D4){
+          gamesRecord.DisplayGamesHistory();
+       }
       else { continue; }
       WriteLine ("(click any key to continue)");
       ReadKey(true);
